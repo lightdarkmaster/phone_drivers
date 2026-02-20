87d6a2e7e764543e88c7d77d8dfc2c29960ead8b
lightdarkmaster <christian.barbosa05222001@gmail.com>
2026-02-20 22:48:26 +0800

    Add ARM32 bit architecture

---
+++ architecture/arm32v7a.py
@@ -0,0 +1,45 @@
+class ARM32V7A:
+    """
+    ARM32V7A architecture complex
+    """
+
+    def __init__(self):
+        self.instruction_set_architecture = "ARMv7-A"
+        self.execute_unit_architecture = "ARMv7-A"
+        self.memory_management_unit_architecture = "ARMv7-A"
+        self.load_store_unit_architecture = "ARMv7-A"
+        self.branch_prediction_unit_architecture = "ARMv7-A"
+        self.floating_point_unit_architecture = "NEON"
+        self.graphics_processing_unit_architecture = "Mali-G71 MP8"
+
+    def get_instruction_set_architecture(self):
+        return self.instruction_set_architecture
+
+    def get_execute_unit_architecture(self):
+        return self.execute_unit_architecture
+
+    def get_memory_management_unit_architecture(self):
+        return self.memory_management_unit_architecture
+
+    def get_load_store_unit_architecture(self):
+        return self.load_store_unit_architecture
+
+    def get_branch_prediction_unit_architecture(self):
+        return self.branch_prediction_unit_architecture
+
+    def get_floating_point_unit_architecture(self):
+        return self.floating_point_unit_architecture
+
+    def get_graphics_processing_unit_architecture(self):
+        return self.graphics_processing_unit_architecture
+
+    def get_all_features(self):
+        return {
+            "Instruction Set Architecture": self.get_instruction_set_architecture(),
+            "Execute Unit Architecture": self.get_execute_unit_architecture(),
+            "Memory Management Unit Architecture": self.get_memory_management_unit_architecture(),
+            "Load/Store Unit Architecture": self.get_load_store_unit_architecture(),
+            "Branch Prediction Unit Architecture": self.get_branch_prediction_unit_architecture(),
+            "Floating Point Unit Architecture": self.get_floating_point_unit_architecture(),
+            "Graphics Processing Unit Architecture": self.get_graphics_processing_unit_architecture()
+        }
