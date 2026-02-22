ee12c07179e250293f913de998759b6edd11b59d
lightdarkmaster <christian.barbosa05222001@gmail.com>
2026-02-22 08:40:44 +0800

    Update Code

--- model/kernelModel.py
+++ model/kernelModel.py
@@ -1,20 +1,35 @@
 class PhoneDriverController:
     def __init__(self):
-        self.camera = None
-        self.file_storage = None
+        self._camera = None
+        self._file_storage = None
 
-    def set_camera(self, camera):
-        self.camera = camera
+    @property
+    def camera(self):
+        return self._camera
 
-    def set_file_storage(self, file_storage):
-        self.file_storage = file_storage
+    @camera.setter
+    def camera(self, value):
+        self._camera = value
+
+    @property
+    def file_storage(self):
+        return self._file_storage
+
+    @file_storage.setter
+    def file_storage(self, value):
+        self._file_storage = value
 
     def take_picture(self):
-        if self.camera is None:
-            raise Exception("Camera not set")
-        return self.camera.take_picture()
+        if self._camera is None:
+            raise ValueError("Camera not set")
+        return self._camera.take_picture()
 
     def save_picture(self, picture):
-        if self.file_storage is None:
-            raise Exception("File storage not set")
-        return self.file_storage.save_picture(picture)
+        if self._file_storage is None:
+            raise ValueError("File storage not set")
+        return self._file_storage.save_picture(picture)
+
+    def capture_and_save(self):
+        picture = self.take_picture()
+        return self.save_picture(picture)
+
